a7591e553774da54098f45c10b167674cf81ca40
jaivrat <jaivrat.singh@gmail.com>
2018-01-27 15:38:28 +0800

    COMPOSE

--- examples/usefulFunctions.R
+++ examples/usefulFunctions.R
@@ -144,3 +144,19 @@ some(1:20, function(x){
 
 
 
+##----------------------  COMPOSE  ---------------------- ##
+
+# Finally, the compose() function combines any number of functions into one function:
+
+n_unique <- compose(length, unique)
+
+# The composition above is the same as:
+# n_unique <- function(x){
+#   length(unique(x))
+# }
+
+rep(1:5, 1:5)
+
+n_unique(rep(1:5, 1:5))
+
+

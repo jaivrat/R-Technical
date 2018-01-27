f1fbee275c5cf303713d50275b88d963f65180f7
jaivrat <jaivrat.singh@gmail.com>
2018-01-27 20:26:48 +0800

    Partial Application

--- examples/usefulFunctions.R
+++ examples/usefulFunctions.R
@@ -160,3 +160,26 @@ rep(1:5, 1:5)
 n_unique(rep(1:5, 1:5))
 
 
+
+##----------------------  PARTIAL APPLICATION  ---------------------- ##
+#Partial application of functions can allow functions to behave a little like data structures. Using the partial() function from the purrrpackage you can specify some of the arguments of a function, and then partial() will return a function that only takes the unspecified arguments. Let’s take a look at a simple example:
+
+mult_three_n <- function(x, y, z){
+	  x * y * z
+}
+
+mult_by_15 <- partial(mult_three_n, x = 3, y = 5)
+
+mult_by_15(z = 4)
+
+
+#By using partial application you can bind some data to the arguments of a function before using that function elsewhere.
+
+
+
+
+
+
+
+
+
